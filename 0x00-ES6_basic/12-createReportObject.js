050c01dd0831359c20163d0595c9efa7d023add2
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-05-30 01:03:20 +0300

    c

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,8 +1,10 @@
 export default function createReportObject(employeesList) {
   return {
-    allEmployees: { ...employeesList },
-    getNumberOfDepartments(list) {
-      return Object.keys(list).length;
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
     },
   };
 }

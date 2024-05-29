9ce50ae52e3d0fd970fa0d088ac546c75439f97e
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-05-30 01:19:11 +0300

    c

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,12 +2,12 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-  /* e */
+  /* eslint-disable no-unused-vars */
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
   }
-  /* e */
+  /* eslint-enable no-unused-vars */
 
   return [task, task2];
 }

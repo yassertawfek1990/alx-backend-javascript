45abb0600894ff10a380fccbdfecaa7346ceb5c9
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-05-29 23:22:31 +0300

    c

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,9 @@
+// con{
+//     ret;
+// }
+
+// ex;
+
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
+}

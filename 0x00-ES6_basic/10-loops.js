e11025130325ecb6dfe79284bbadfff95c605e9f
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-05-30 00:07:59 +0300

    c

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  for (const value of array) {
+    const idx = array.indexOf(value);
+    array[idx] = appendString + value; // es
+  }
+
+  return array;
+}

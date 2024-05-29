0c4e156d2233641aaa2cec93d565505c545efbb8
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-05-30 01:55:51 +0300

    c

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,16 +2,15 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const results = await Promise.allSettled([
+  const promises = [
     signUpUser(firstName, lastName),
     uploadPhoto(fileName)
-  ]);
+  ];
 
-  return results.map(result => {
-    if (result.status === 'fulfilled') {
-      return { status: result.status, value: result.value };
-    } else {
-      return { status: result.status, value: String(result.reason) };
-    }
-  });
+  const results = await Promise.allSettled(promises);
+
+  return results.map(result => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : String(result.reason),
+  }));
 }

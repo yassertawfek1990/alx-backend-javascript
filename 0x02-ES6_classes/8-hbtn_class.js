be7b2634700f0d9ee5068de2e5bb91ffec25f1b7
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-05-30 22:56:38 +0300

    c

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,40 @@
+class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Size must be a number');
+    }
+    this._size = value;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  set location(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Location must be a string');
+    }
+    this._location = value;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
+    if (hint === 'string') {
+      return this._location;
+    }
+    return null;
+  }
+}
+
+export default HolbertonClass;

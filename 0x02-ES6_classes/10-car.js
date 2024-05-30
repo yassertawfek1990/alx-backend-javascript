0f78d2ad0a7238420f8a30687a87aac77e9c13d8
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-05-30 23:00:38 +0300

    c

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,49 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  set brand(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Brand must be a string');
+    }
+    this._brand = value;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  set motor(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Motor must be a string');
+    }
+    this._motor = value;
+  }
+
+  get color() {
+    return this._color;
+  }
+
+  set color(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Color must be a string');
+    }
+    this._color = value;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    return new Species(this._brand, this._motor, this._color);
+  }
+}

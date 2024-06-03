e313eac102c59cdf1b95dd8f2e0032834334bbbe
yasssertawfek1990 <dr.yassertawfek@gmail.com>
2024-06-03 23:06:52 +0300

    c

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+export default function getListStudents() {
+  const a = {
+    id: 1,
+    firstName: 'Guillaume',
+    location: 'San Francisco',
+  };
+
+  const s = {
+    id: 2,
+    firstName: 'James',
+    location: 'Columbia',
+  };
+
+  const d = {
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
+  };
+
+  return [a, s, d];
+}
--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,21 +1,8 @@
-export default function getListStudents() {
-  const a = {
-    id: 1,
-    firstName: 'Guillaume',
-    location: 'San Francisco',
-  };
+export default function getListStudentIds(arr) {
+  let r = [];
+  if (arr instanceof Array) {
+    r = arr.map((item) => item.id);
+  }
 
-  const s = {
-    id: 2,
-    firstName: 'James',
-    location: 'Columbia',
-  };
-
-  const d = {
-    id: 5,
-    firstName: 'Serena',
-    location: 'San Francisco',
-  };
-
-  return [a, s, d];
+  return r;
 }
